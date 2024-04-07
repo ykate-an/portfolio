42a14eb81486ebf89f826e400a1e623e567a07aa
= <yeokyeong1004@gmail.com>
2024-04-07 23:09:43 +0100

    [kate] git hub pages setting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,6 @@ import "dotenv/config";
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 const config: GatsbyConfig = {
-  pathPrefix: "/ykate-an.github.io",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
